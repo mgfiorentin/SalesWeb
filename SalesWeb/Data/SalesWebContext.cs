90e93f475af28d76a1f123944db3b42c033ffd70
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-16 09:06:25 -0300

    Finalizadas as entidades

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -8,11 +8,16 @@ namespace SalesWeb.Models
 {
     public class SalesWebContext : DbContext
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+        public SalesWebContext(DbContextOptions<SalesWebContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
+
+
+
 }
--- SalesWeb/Program.cs
+++ SalesWeb/Program.cs
@@ -14,6 +14,7 @@ namespace SalesWeb
     {
         public static void Main(string[] args)
         {
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
