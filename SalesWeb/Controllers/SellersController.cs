35e2c70d64d549b40d278a009d24e68d5992173f
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-20 09:36:58 -0300

    Delete seller

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -28,11 +28,12 @@ namespace SalesWeb.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments};
+            var viewModel = new SellerFormViewModel { Departments = departments };
 
             return View(viewModel);
 
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -41,5 +42,31 @@ namespace SalesWeb.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null) return NotFound();
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            
+
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
