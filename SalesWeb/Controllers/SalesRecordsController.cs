68409df2bdedd0177da4a7f104525d657ca53447
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-22 16:15:14 -0300

    Grouping search

--- SalesWeb/Controllers/SalesRecordsController.cs
+++ SalesWeb/Controllers/SalesRecordsController.cs
@@ -31,9 +31,18 @@ namespace SalesWeb.Controllers
             return View(sales);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue) minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            if (!maxDate.HasValue) maxDate = DateTime.Now;
+            var sales = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            return View(sales);
+
+            
         }
     }
 }
