b9abab3b1fd9beb1bc76fdf27a478a57a442276a
mgfiorentin <matheus.fiorentin@gmail.com>
2022-06-23 13:52:25 -0300

    Homescreen edited

--- SalesWeb/Controllers/DepartmentsController.cs
+++ SalesWeb/Controllers/DepartmentsController.cs
@@ -49,8 +49,7 @@ namespace SalesWeb.Controllers
         }
 
         // POST: Departments/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name")] Department department)
@@ -81,8 +80,7 @@ namespace SalesWeb.Controllers
         }
 
         // POST: Departments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Department department)
